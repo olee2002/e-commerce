063febd5cc5a68e21c307df929708d386dd486db
babylee2002 <babylee2002@gmail.com>
2020-07-19 15:04:40 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/olee2002.github.i/e-commerce/precache-manifest.de450df238ef1b5eb716c91b886fcd8d.js"
+  "/olee2002.github.io/e-commerce/precache-manifest.0d47abb533aabf2dc687ce0dad228610.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/olee2002.github.i/e-commerce/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/olee2002.github.io/e-commerce/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
