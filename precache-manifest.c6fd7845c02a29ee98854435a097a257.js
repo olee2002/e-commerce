1ae8e5159b9d61aaf39d167edefcdffc00337474
babylee2002 <babylee2002@gmail.com>
2020-07-19 15:07:27 -0400

    Updates

---
+++ precache-manifest.c6fd7845c02a29ee98854435a097a257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2725972973fe33a752332d2782aa92f",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "2869d2129c41de54cfd7",
+    "url": "/e-commerce/static/css/main.af6cc072.chunk.css"
+  },
+  {
+    "revision": "9ff2aa91293f120c5eb3",
+    "url": "/e-commerce/static/js/2.fefdf28d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-commerce/static/js/2.fefdf28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2869d2129c41de54cfd7",
+    "url": "/e-commerce/static/js/main.eebb50b9.chunk.js"
+  },
+  {
+    "revision": "ebcbe2fd560d989b284d",
+    "url": "/e-commerce/static/js/runtime-main.9cf8524b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/olee2002.github.io/e-commerce/precache-manifest.0d47abb533aabf2dc687ce0dad228610.js"
+  "/e-commerce/precache-manifest.c6fd7845c02a29ee98854435a097a257.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/olee2002.github.io/e-commerce/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-commerce/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
