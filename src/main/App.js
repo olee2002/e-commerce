07e6b832995e6aa183f2fd8a08586b593b80f43c
babylee2002 <babylee2002@gmail.com>
2020-07-21 20:31:23 -0400

    updated base route

--- src/main/App.js
+++ src/main/App.js
@@ -14,6 +14,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/e-commerce' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
         <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
