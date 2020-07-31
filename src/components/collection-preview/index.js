c2adde1fdcdecb3c597db1c94a923022ab9badd5
babylee2002 <babylee2002@gmail.com>
2020-07-31 02:13:56 -0400

    i

--- src/components/collection-preview/index.js
+++ src/components/collection-preview/index.js
@@ -9,7 +9,7 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {items
-        .filter((item, idx) => idx < 4)
+        .filter((item, i) => i < 4)
         .map(({ id, ...otherItemProps }) => (
           <CollectionItem key={id} {...otherItemProps} />
         ))}
