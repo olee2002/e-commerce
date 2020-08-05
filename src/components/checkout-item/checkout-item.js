9f9a310d7c8136b156adce9efb497befec46fc3b
babylee2002 <babylee2002@gmail.com>
2020-08-05 00:14:02 -0400

    checkout is working

--- src/components/checkout-item/checkout-item.js
+++ src/components/checkout-item/checkout-item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import {
   clearItemFromCart,
@@ -9,7 +9,8 @@ import {
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+const CheckoutItem = ({ cartItem }) => {
+   const dispatch = useDispatch();
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
@@ -18,16 +19,16 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
       </div>
       <span className='name'>{name}</span>
       <span className='quantity'>
-        <div className='arrow' onClick={() => removeItem(cartItem)}>
+        <div className='arrow' onClick={() => dispatch(removeItem(cartItem))}>
           &#10094;
         </div>
         <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={() => addItem(cartItem)}>
+        <div className='arrow' onClick={() => dispatch(addItem(cartItem))}>
           &#10095;
         </div>
       </span>
       <span className='price'>{price}</span>
-      <div className='remove-button' onClick={() => clearItem(cartItem)}>
+      <div className='remove-button' onClick={() => dispatch(clearItemFromCart(cartItem))}>
         &#10005;
       </div>
     </div>
--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,41 +1,53 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
-import CheckoutItem from '../../components/checkout-item/checkout-item';
-
+import CheckoutItem from '../../components/checkout-item/checkout-item'
 import {
-  selectCartItems,
-  selectCartTotal
-} from '../../redux/cart/cart.selectors';
+   selectCartItems,
+   selectCartTotal,
+} from '../../redux/cart/cart.selectors'
 
-import './checkout.styles.scss';
+import './checkout.styles.scss'
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className='checkout-page'>
-    <div className='checkout-header'>
-      <div className='header-block'>
-        <span>Product</span>
-      </div>
-      <div className='header-block'>
-        <span>Description</span>
-      </div>
-      <div className='header-block'>
-        <span>Quantity</span>
-      </div>
-      <div className='header-block'>
-        <span>Price</span>
-      </div>
-      <div className='header-block'>
-        <span>Remove</span>
-      </div>
-    </div>
-    {cartItems ? cartItems.length>0 && cartItems.map(cartItem => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    )): <div style={{ marginTop: 20 }}>Current No Item is selected. Please go back to the shop page!</div>}
-    <div className='total'>TOTAL: ${total}</div>
-  </div>
-);
+const CheckoutPage = () => {
+   const dispatch = useDispatch();
+   const cart = useSelector(state=>state.cart);
+   const { cartItems } = cart;
+   const total = cartItems && cartItems.length>0 ? cartItems.map(item=>(item.price * item.quantity)).reduce((a,b)=>a+b): 0 ;
 
 
-export default CheckoutPage;
+   return (
+      <div className='checkout-page'>
+         <div className='checkout-header'>
+            <div className='header-block'>
+               <span>Product</span>
+            </div>
+            <div className='header-block'>
+               <span>Description</span>
+            </div>
+            <div className='header-block'>
+               <span>Quantity</span>
+            </div>
+            <div className='header-block'>
+               <span>Price</span>
+            </div>
+            <div className='header-block'>
+               <span>Remove</span>
+            </div>
+         </div>
+         {cartItems ? (
+            cartItems.length > 0 &&
+            cartItems.map((cartItem) => (
+               <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))
+         ) : (
+            <div style={{ marginTop: 20 }}>
+               Current No Item is selected. Please go back to the shop page!
+            </div>
+         )}
+         <div className='total'>TOTAL: ${total}</div>
+      </div>
+   )
+}
+
+export default CheckoutPage
