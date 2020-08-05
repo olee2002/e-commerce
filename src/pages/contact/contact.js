42e7c120256de55126b2a96b9a36a8cc4efc72eb
babylee2002 <babylee2002@gmail.com>
2020-08-05 12:36:37 -0400

    black cat added

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -8,7 +8,7 @@ import './contact.scss';
 const SignInAndSignUpPage = ({ getSignInInfo }) => (
   <div className='contact'>
      <div className='line'/>
-     <img className='cat-box' style={{width: 600}} src="https://res.allmacwallpaper.com/get/Retina-MacBook-Air-13-inch-wallpapers/Black-Cat-2560x1600/9618-11.jpg"/>
+     <img className='cat-box' style={{width: 600}} src="./black-cat.jpg"/>
     <div className='text'>This site is currently being built. Any inquiries about the site please contact "Dev O'lee" @ babylee2002@gmail.com</div>
     <div className='line'/>
   </div>
