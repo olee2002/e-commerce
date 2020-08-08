053a781b3f182b7179722839b9924ca33c30e8d1
babylee2002 <babylee2002@gmail.com>
2020-08-08 11:12:29 -0400

    shopping cart behavior improved

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -9,23 +9,27 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions.js'
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = ({ cartItems, history, dispatch, handleCancel }) => (
-   <div className='cart-dropdown' onMouseLeave={handleCancel}>
+   <div className='cart-dropdown' onMouseLeave={handleCancel} style={{ height: cartItems && cartItems.length> 3 ? '490px':'400px' }}>
       <div className='cart-cancel' onClick={handleCancel}>
          X
       </div>
       <div className='cart-items'>
+         {cartItems && cartItems.length > 0 && (
+            <div className='message'>
+               You have <strong>{cartItems.length}</strong> item(s) in your
+               shopping cart.
+            </div>
+         )}
          {cartItems.length ? (
             cartItems.map((cartItem) => (
                <CartItem key={cartItem.id} item={cartItem} />
             ))
          ) : (
-            <span className='empty-message'>Your cart is empty</span>
+            <span className='empty-message'>Your cart is empty.</span>
          )}
       </div>
       <Link className='link-btn-container' to='/e-commerce/checkout'>
-         <CustomButton onClick={() => toggleCartHidden()}>
-            GO TO CHECKOUT
-         </CustomButton>
+         <CustomButton onClick={handleCancel}>GO TO CHECKOUT</CustomButton>
       </Link>
    </div>
 )
