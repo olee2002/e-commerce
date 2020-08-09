b83b4545306a941fc385279c335b8645e3709382
babylee2002 <babylee2002@gmail.com>
2020-08-09 15:02:27 -0400

    added to the cart

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline'
 
-import CustomButton from '../custom-button/custom-button';
-import { addItem } from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button'
+import { addItem } from '../../redux/cart/cart.actions'
 
-import './collection-item.scss';
+import './collection-item.scss'
 
 const CollectionItem = (props) => {
-   const dispatch = useDispatch();
-   const cartItems = useSelector(state=>state.cart.cartItems);
-   const { id, name, price, imageUrl } = props.item;
-   const isSelected = cartItems && cartItems[0] && cartItems.map(item=>item.id).includes(id);
+   const dispatch = useDispatch()
+   const cartItems = useSelector((state) => state.cart.cartItems)
+   const { id, name, price, imageUrl } = props.item
+   const isSelected =
+      cartItems && cartItems[0] && cartItems.map((item) => item.id).includes(id)
    return (
       <div className='collection-item'>
-      <div
-        className='image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+         <div
+            className='image'
+            style={{
+               backgroundImage: `url(${imageUrl})`,
+            }}
+         />
+         <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+         </div>
+         <CustomButton onClick={() => dispatch(addItem(props.item))} inverted>
+            {isSelected ? 'Added' : 'Add'} to cart{' '}
+            {isSelected ? (
+               <CheckCircleOutlineIcon
+                  style={{ color: 'green', marginLeft: '5px' }}
+                  fontSize='large'
+               />
+            ) : null}
+         </CustomButton>
       </div>
-      <CustomButton onClick={() => dispatch(addItem(props.item))} inverted>
-        Add to cart { isSelected ? <CheckCircleOutlineIcon style={{color:'green', marginLeft: '5px'}} fontSize='large'/> : null }
-      </CustomButton>
-    </div>
-);
-   }
+   )
+}
 
-export default CollectionItem;
+export default CollectionItem
