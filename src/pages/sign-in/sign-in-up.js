d66a727e322e734a1a75fcb3041744348d2486d4
babylee2002 <babylee2002@gmail.com>
2020-08-28 19:24:53 -0400

    updating sign in page

--- src/pages/sign-in/sign-in-up.js
+++ src/pages/sign-in/sign-in-up.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-import SignIn from '../../components/sign-in/sign-in';
-import SignUp from '../../components/sign-up/sign-up';
-import { auth } from '../../firebase/firebase.utils';
+import SignIn from '../../components/sign-in/sign-in'
+import SignUp from '../../components/sign-up/sign-up'
+import { auth } from '../../firebase/firebase.utils'
 
-import './sign-in.scss';
+import './sign-in.scss'
 
 const SignInAndSignUpPage = ({ displayName, getSignInInfo }) => {
-   const { currentUser } = auth;
-   const history = useHistory();
-   if(currentUser && currentUser.email){
-      console.log('currentUser.email',currentUser.email)
+   const [register, setRegister] = useState(false)
+   const { currentUser } = auth
+   const history = useHistory()
+   if (currentUser && currentUser.email) {
+      console.log('currentUser.email', currentUser.email)
       history.push('/e-commerce')
-   } 
-   return (
-  <div className='sign-in-and-sign-up'>
-    <SignIn getSignInInfo={getSignInInfo} />
-    {!displayName && <SignUp />}
-  </div>
-);
    }
+   return (
+      <div className='sign-in-and-sign-up'>
+         <button className='toggle-btn' onClick={() => setRegister(!register)}>{!register ?'Register' :'SignIn'}</button>
+         <div>
+            {!register ? <SignIn getSignInInfo={getSignInInfo} /> : <SignUp />}
+         </div>
+      </div>
+   )
+}
 
-export default SignInAndSignUpPage;
+export default SignInAndSignUpPage
