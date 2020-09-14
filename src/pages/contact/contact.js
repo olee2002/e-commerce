408619b72447a44407fd7ec1d165c130e5b2deb7
babylee2002 <babylee2002@gmail.com>
2020-09-13 21:32:07 -0400

    add margin

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -4,10 +4,9 @@ import './contact.scss';
 
 const Contact = ({ getSignInInfo }) => (
   <div className='contact'>
-     <div className='line'/>
      <img className='cat-box' src="./black-cat.jpg"/>
+     <div className='line'/>
     <div className='text'>This site is currently being built. Any inquiries about the site please contact "Dev O'lee" @ babylee2002@gmail.com</div>
-    <div className='line'/>
   </div>
 );
 
