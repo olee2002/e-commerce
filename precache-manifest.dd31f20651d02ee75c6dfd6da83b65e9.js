a98025f6e0f6eba5e9fb7b39c30c0cd6165234b4
babylee2002 <babylee2002@gmail.com>
2020-09-25 12:34:39 -0400

    Updates

---
+++ precache-manifest.dd31f20651d02ee75c6dfd6da83b65e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2794aba6547e71577472e412659a038c",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "42ddbaf2a974bbc72220",
+    "url": "/e-commerce/static/css/main.cc3c891b.chunk.css"
+  },
+  {
+    "revision": "b9e592c932bf3c2418ff",
+    "url": "/e-commerce/static/js/2.fe877096.chunk.js"
+  },
+  {
+    "revision": "6c8202bc20827f0979ac9cc13f2fae8d",
+    "url": "/e-commerce/static/js/2.fe877096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ddbaf2a974bbc72220",
+    "url": "/e-commerce/static/js/main.782d84c7.chunk.js"
+  },
+  {
+    "revision": "7c010014b3292c6e9d87",
+    "url": "/e-commerce/static/js/runtime-main.7fd9d710.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/e-commerce/precache-manifest.13b490bc277e59ef366812ec277bc8a8.js"
+  "/e-commerce/precache-manifest.dd31f20651d02ee75c6dfd6da83b65e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-commerce/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
